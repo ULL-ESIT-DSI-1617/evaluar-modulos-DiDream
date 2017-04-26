cb15fa2408d59f7993cdb7af262c062bff3d5a74
Alex Jimmy <alu0100728170@ull.edu.es>
2017-04-26 12:41:25 +0100

    Bugs fixed

--- bin/team-area.js
+++ bin/team-area.js
@@ -1,12 +1,11 @@
 #! /usr/bin/env node
 'use strict'
 let util = require('util');
-let shape = require('../shapesarea');
 
 try {
     let type=process.argv[2];
-    let Module = require(`@didream/ull-shape-${type.toLowerCase()}`);
-    let obj = JSON.parse(process.argv[3].toString());
+    let Shape = require(`ull-shape-${type.toLowerCase()}`);
+    let obj = eval('('+process.argv[3]+')');
     let t= new Shape(obj,type);
     let s= t.getArea();
     console.log(s);
