35ee92f5757a21befa6d56e64760ff84b54fcc92
Alex Jimmy <alu0100728170@ull.edu.es>
2017-04-27 01:21:27 +0100

    Ejecutable valido para tests

--- bin/team-area.js
+++ bin/team-area.js
@@ -2,9 +2,18 @@
 'use strict'
 let util = require('util');
 let Shape = require('@didream/ull-shape');
+let execSync = require('child_process').execSync;
 try {
     let type=process.argv[2];
-    let Module = require(`@didream/ull-shape-${type.toLowerCase()}`);
+    var modulePath = '@didream/ull-shape-';
+    if(process.argv[4] === 'test'){
+
+        //return node_modules path
+        var node_modulesPath = execSync('npm root',{encoding:'utf-8'});
+        modulePath = `${node_modulesPath}/../`;
+    }
+
+    let Module = require(modulePath + type.toLowerCase());
     let obj = eval('('+process.argv[3]+')');
     let t= new Shape(obj,type);
     let s= t.getArea();
